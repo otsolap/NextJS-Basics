401fd4ef2ce7663e469c92b0dbae11abd6f687a8
OtsoLap <otsolappalainen@gmail.com>
2022-03-10 21:09:28 +0200

    bug fix

--- stores/authContext.js
+++ stores/authContext.js
@@ -12,7 +12,7 @@ export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null)
 
     useEffect(() => {
-        netlifyIdentity.on('login', () => {
+        netlifyIdentity.on('login', (user) => {
             setUser(user)
             netlifyIdentity.close()
             console.log('Login Event triggered')
